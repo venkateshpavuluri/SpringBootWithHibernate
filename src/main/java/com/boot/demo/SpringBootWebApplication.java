9d984d1e9241e453ba7e67cb9797b6ca6d69370d
Venkatesh <pavuluri.venki@gmail.com>
2018-02-25 16:48:10 +0530

    Packages changed

---
+++ src/main/java/com/boot/demo/SpringBootWebApplication.java
@@ -0,0 +1,24 @@
+package com.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@ComponentScan("com")
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWebApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
